937631c298042ec5f945619cb176d8dc23ac64bf
Javier_Sanguino_Espana <fjse0505@gmail.com>
2021-08-17 10:17:08 -0500

    Creacion se la seccion paginacion

--- links/paginacion.php
+++ links/paginacion.php
@@ -6,24 +6,46 @@ include("../php/coneccion.php");
 $count = 0;
 $link = ['wolverine2', 'batman', 'superman', 'capitanAmerica', 'venom', 'ironman', 'wolverine', 'wonderWoman', 'daenerys'];
 
-$sql = "SELECT * FROM sesion where user='" . $_SESSION['usuario'] . "'";
+$sql = "SELECT COUNT(id) FROM clientes";
 foreach ($conn->query($sql) as $fila) {
-    $foto = $fila[10];
+    $registros = $fila[0];
 }
-$sql = "SELECT * FROM funkos where 1";
+//echo $registros;
+
+echo "Limite 1: " . $_GET['limite1'];
+echo "<br>";
+echo "Limite 2: " . $_GET['limite2'];
+echo "<br>";
+
+$sql = "SELECT * FROM clientes LIMIT " . $_GET['limite1'] . ", " . $_GET['limite2'];
 foreach ($conn->query($sql) as $fila) {
     $id[$count] = $fila[0];
     $nombre[$count] = $fila[1];
-    $descripcion[$count] = $fila[2];
-    $price[$count] = $fila[3];
-    $imagen[$count] = $fila[4];
+    $apellidos[$count] = $fila[2];
     $count++;
 }
+
+// echo "URL: " . $sql;
+// echo "<br>";
+
+for ($i = 0; $i < $count; $i++) {
+    echo $id[$i];
+    echo "  ";
+    echo $nombre[$i];
+    echo "  ";
+    echo $apellidos[$i];
+    echo "<br>";
+}
+
+$id = null;
+$nombre = null;
+$apellidos = null;
 
 if (!isset($_SESSION['usuario'])) {
     echo "Por favor inicie sesion primero: ";
     echo "<a href='../links/sesion.html'>Inicio sesion</a>";
 } else {
+
 
 ?>
 
@@ -42,7 +64,7 @@ if (!isset($_SESSION['usuario'])) {
     </head>
 
     <body>
-        <nav class="menu_bar">
+        <!-- <nav class="menu_bar">
             <ul class="menu">
                 <li class="menu_item"><a href="../index.html">Home</a></li>
                 <li class="menu_item"><a href="./conocenos.html">Con&oacute;cenos</a></li>
@@ -55,60 +77,19 @@ if (!isset($_SESSION['usuario'])) {
                     </ul>
                 </li>
             </ul>
-        </nav>
+        </nav> -->
         <h1>Bienvenido a compras <?php echo $_SESSION['nombre']; ?></h1>
-        <section class="item_contenedor">
-            <form action="../php/ejecutar_compra.php" method="POST" name="form_item-selected" class="form_contenedor">
-                <?php
-                for ($i = 0; $i < $count; $i++) {
-                    echo "<div class='item'>";
-                    echo "<div class='item_img'>
-                    <a href='./shop_links/" . $link[$i] . ".html' target='window' onclick='windowFunction()'>
-                    <img src='" . $imagen[$i] . "' alt='" . $nombre[$i] . "'></div>
-                    </a>";
-                    echo "<div class='item_description'>";
-                    echo "<h3>funko " . $nombre[$i] . "</h3>";
-                    echo "<p>" . $descripcion[$i] . "</p>";
-                    echo "<h4>$ " . $price[$i] . "</h4>";
-                    echo "</div>";
-                    echo "<div class='item_cant'>";
-                    echo "<p>Cantidad: </p>";
-                    echo "<input type='number' value='1' name='cant" . $i . "'>";
-                    echo "</div>";
-                    echo "<input type='checkbox' value='" . $id[$i] . "' name=" . $i . " class='item_selected'>";
-                    echo "</div>";
-                }
-                echo "<input type='hidden' name='cont' value='" . $i . "'>";
-                ?>
-                <div class="comprar_contenedor">
-                    <input type="submit" name="submit" value="Comprar" class="btn_comprar">
-                </div>
-            </form>
-        </section>
-        <section class="iframe_contenedor" id="iframeContenedor">
-            <div class="btn_exit" onclick="windowClose()">
-                <i class="fas fa-times"></i>
-            </div>
-            <iframe src="" frameborder="0" name="window"></iframe>
-        </section>
         <div>
             <ul class="pagination">
-            <?php
-            $sql = "SELECT COUNT(id) FROM clientes";
-            foreach ($conn->query($sql) as $fila) {
-                $registros = $fila[0];
-            }
+                <?php
                 $limit1 = 0;
-                echo "<li class='page-item disabled'><a class='page-link' href='#'>Previous</a></li>";
                 for ($i = 1; $i < $registros / 10; $i++) {
-                    $limit2 = ($i * 10);
                     echo "<li class='page-item'><a class='page-link' href='./paginacion.php?limite1=" . $limit1 . "&limite2=10'>" . $i . "</a></li>";
                     $limit1 = ($i * 10) + 1;
+                    $previous = ($limit1) - 10;
                 }
-                $limit2 += 1;
-                //echo "<li class='page-item'><a class='page-link' href='./paginacion.php?1=10'>" . $limit2 . " " . $registros . "</a></li>";
                 echo "<li class='page-item'><a class='page-link' href='./paginacion.php?limite1=" . $limit1 . "&limite2=10'>" . $i . "</a></li>";
-
+                echo "<li class='page-item'><a class='page-link' href='./paginacion.php?limite1=" . $previous . "&limite2=10'>Previous</a></li>";
                 ?>
             </ul>
 
@@ -119,7 +100,7 @@ if (!isset($_SESSION['usuario'])) {
         <script src="../JS/shop.js"></script>
         <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
     </body>
-    
+
 <?php
 
 }
